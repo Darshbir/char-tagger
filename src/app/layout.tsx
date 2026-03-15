d0504a4c38e1b8f8b91f9086cb2146b0ed12746e
Darshbir Singh <darshbir86@gmail.com>
2026-03-15 22:34:44 +0530

    Google Verification Meta Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="UfdW5J9SxXxpMfNswvammpnPx0IUFrlLaRCXtu33KrM" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
